de30d27ab1ef85d59499909d8135338c1f0cb500
ASUS <ASUS@Anas>
2018-12-16 19:02:12 +0700

    Tugas Socket

---
+++ src/ChatSocket/ChatSocket.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ChatSocket;
+
+
+public class ChatSocket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Server s = new Server();
+        s.setVisible(true);
+        // TODO code application logic here
+    }
+}
